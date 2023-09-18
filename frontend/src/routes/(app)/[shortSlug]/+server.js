8fff5b89463f2e485e7b95b0344b8d34b897c6f9
Davies Brown <nwosudavid77.com>
2023-09-18 22:10:46 +0100

    feat: endpoint for getting urls from shortslug

--- frontend/src/routes/(app)/[shortSlug]/+server.js
+++ frontend/src/routes/(app)/[shortSlug]/+server.js
@@ -1,19 +1,21 @@
-import { redirect } from "@sveltejs/kit"
+import { redirect } from "@sveltejs/kit";
 
+// GET endpoint to get a link from its shortslug 
+// and update the number of clicks
 export const GET = async ({ locals, params }) => {
   let link;
   try {
     link = await locals.pb
       .collection("links")
-      .getFirstListItem(`shortSlug="${params.shortSlug}"`)
+      .getFirstListItem(`shortSlug="${params.shortSlug}"`);
 
     await locals.pb.collection("links").update(link.id, {
       clicks: link.clicks + 1,
-    })
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-
-  if (link?.url) throw redirect(303, link?.url)
-  throw redirect(303, "/")
+  // redirect browser to url if available
+  if (link?.url) throw redirect(303, link?.url);
+  throw redirect(303, "/");
 }
